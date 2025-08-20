a38ff89e3f8a1c34ca8c4d125aa78243eea38b93
Jon Seidmann <jons@fractyl.com>
2025-08-20 07:30:35 -0400

    fix: add base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Linear-actuator/',
   plugins: [react()],
   define: {
     // Expose build-time environment variables to the client-side code.
